3654edcf037ac6090d750ebb4a84687beeebcef0
Faintbird101 <kinyuaviktar@gmail.com>
2024-06-13 19:00:28 +0300

    key_api

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -4,9 +4,9 @@ import 'dart:developer';
 import 'package:mohoro/common.libs.dart';
 
 class NewsRepository {
-  static String mainUrl = "YOUR-MAIN-URL"; //**REPLACE THIS WITH YOUR MAIN URL**//
-  static String localUrl = "YOUR-LOCAL-URL"; //**REPLACE THIS WITH YOUR LOCAL URL**//
-  final String apiKey = "YOUR-API-KEY"; //**REPLACE THIS WITH YOUR API KEY**//
+  static String mainUrl = "https://newsapi.org/v2/";
+  static String localUrl = "http://localhost:8080";
+  final String apiKey = "6a9d18fad44e475fbfe072ee01c207bf";
 
   final Dio _dio = Dio();
 
